82107fde4cf53352117ada6a307aef1b4e5a32f0
kishore <skill2automate@gmail.com>
2026-06-15 20:24:46 +0530

    adding a test

---
+++ project123/src/test/java/tests/SampleTest1.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest1 {
+
+	@Test
+	void test() {
+	System.out.println("test1");
+	}
+
+}
