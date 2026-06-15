22a7b288f2a080bbf36b80e3f051bb526a5f14cf
kishore <skill2automate@gmail.com>
2026-06-15 20:27:02 +0530

    2 cases added

---
+++ project123/src/test/java/tests/SampleTest2.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest2 {
+
+	@Test
+	void test() {
+	System.out.println("test1");
+	}
+
+}
---
+++ project123/src/test/java/tests/SampleTest3.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest3 {
+
+	@Test
+	void test() {
+	System.out.println("test1");
+	}
+
+}
