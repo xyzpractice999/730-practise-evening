07b6d21c121d6a91abddfcddc97d261905ebb31c
kishore <skill2automate@gmail.com>
2026-06-15 20:42:50 +0530

    msg

---
+++ project123/src/test/java/tests/SampleTest6.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SampleTest6 {
+
+	@Test
+	void test() {
+	System.out.println("test1");
+	}
+
+}
